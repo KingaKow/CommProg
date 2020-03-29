ade094f13fc5a62965073664964df12bc3452c91
Kinga Kowalska <kk83178@student.sgh.waw.pl>
2020-03-30 00:11:43 +0200

    zad1-bowling + zad2 Fibonacci_number

---
+++ zad1.java
@@ -0,0 +1,10 @@
+public class zad1 {
+    public static void main(String[] args) {
+        int rows = 5;
+        int j = 0;
+        for (int i = 0; i <= 5; i++) {
+            j += i;
+        }
+        System.out.println("j=" + j);
+    }
+}
